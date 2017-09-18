592d7232225be58053f03807ef6e0e48034f5125
Brian Nguyen <thuann89@gmail.com>
2017-09-18 21:12:08 +0700

    Add comment exlaination for v1

--- spiper.v1.js
+++ spiper.v1.js
@@ -6,17 +6,21 @@ const utilities = require('./utils');
 
 function spider(url, callback) {
 	const filename = utilities.urlToFilename(url);
-	fs.exists(filename, exists => {        //[1]
+	// Checks if the URL was already downloaded by verifying that the corresponding file was not already created
+	fs.exists(filename, exists => {        
 		if(!exists) {
+			// If the file is not found, the URL is downloaded using the following line of code
 			console.log(`Downloading ${url}`);
-			request(url, (err, response, body) => {      //[2]
+			request(url, (err, response, body) => {      
 				if(err) {
 					callback(err);
 				} else {
+					// Then, we make sure whether the directory that will contain the file exists or not
 					mkdirp(path.dirname(filename), err => {    //[3]
 						if(err) {
 							callback(err);
 						} else {
+							// Finally, we write the body of the HTTP response to the filesystem:
 							fs.writeFile(filename, body, err => { //[4]
 								if(err) {
 									callback(err);
